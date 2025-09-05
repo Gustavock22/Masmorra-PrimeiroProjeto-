461df56ac028066ab547724384c5c80f2342ca00
Gustavo Camargo Martins <104230942+Gustavock22@users.noreply.github.com>
2025-09-04 22:15:26 -0300

    #1 Colocando a Masmorra
    Colocando os códigos da masmorra

---
+++ structs.h
@@ -0,0 +1,19 @@
+#ifndef STRUCT_H //nome da biblioteca
+	#define STRUCT_H //nome da biblioteca
+	
+	typedef struct {
+	    int x;
+	    int y;
+	} Posicao;
+	
+	typedef struct {
+	    int Inv[20];
+	} Inventario;
+	
+	typedef struct{
+	    int resposta;
+	}Responder;
+	
+
+
+#endif
